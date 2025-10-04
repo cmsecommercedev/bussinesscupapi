50a8978bc4e1947e59810e57526f1259525c867d
Gokhan Gok <gokhan.gok@agx-labs.com>
2025-10-04 19:10:20 +0300

    sdfs

---
+++ Migrations/20251004153356_AddStoryImage555dfsdfsdf.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BussinessCupApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStoryImage555dfsdfsdf : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
