86f3e9a404e4cf7eea8bdb9b3dbff801c53c2913
Gokhan Gok <gokhan.gok@agx-labs.com>
2025-10-07 20:47:01 +0300

    sdf

--- Migrations/20251007174643_AddStoryImage555dfsdfsdfrrdfdsd.Designer.cs
+++ Migrations/20251007174643_AddStoryImage555dfsdfsdfrrdfdsd.Designer.cs
@@ -4,6 +4,7 @@ using BussinessCupApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BussinessCupApi.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251007174643_AddStoryImage555dfsdfsdfrrdfdsd")]
+    partial class AddStoryImage555dfsdfsdfrrdfdsd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
